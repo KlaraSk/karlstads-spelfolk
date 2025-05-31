5d0cbe7aae8eb819cf3e28138040c1dc6dd4ec72
Klara Sköld <170510677+KlaraSk@users.noreply.github.com>
2025-05-31 08:26:00 +0200

    Break

---
+++ src/components/list-item-info/ListItemInfo.jsx
@@ -0,0 +1,9 @@
+function ListItemInfo({ info, icon }) {
+  return (
+    <div>
+      <span className="list-item__info">{info}</span>
+    </div>
+  );
+}
+
+export default ListItemInfo;
--- src/components/list-item/ListItem.jsx
+++ src/components/list-item/ListItem.jsx
@@ -1,9 +1,41 @@
+import "./ListItem.css";
+import Sell from "@mui/icons-material/Sell";
+import ListItemInfo from "../list-item-info/ListItemInfo.jsx";
+
 function ListItem({ tune }) {
   return (
-    <li>
+    <li className="list-item">
+      <span className="list-item__nr">{tune.tuneNumber}</span>
       <a href={tune.recording}> {tune.title}</a>
+      <div>
+        <Sell />
+        <span className="list-item__category">{tune.category.main}</span>
+      </div>
+
+      {tune.category.sub && <span className="list-item__category">{tune.category.sub}</span>}
+      {/* <ListItemInfo icon={Sell}></ListItemInfo> */}
     </li>
   );
 }
 
 export default ListItem;
+
+// category
+// :
+// {main: 'Polska', sub: null}
+// composer
+// :
+// null
+// isComposedByUs
+// :
+// false
+// location
+// :
+// "Eda"
+
+// tuneId
+// :
+// "5fg78"
+// tuneNumber
+// :
+// 1
